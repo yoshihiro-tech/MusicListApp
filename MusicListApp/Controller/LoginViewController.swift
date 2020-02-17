777f5a3a64f3ae67b81316ef0a0af5db12be347a
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-02-17 19:11:32 +0900

    add SaveProfile model

---
+++ MusicListApp/Controller/LoginViewController.swift
@@ -0,0 +1,87 @@
+//
+//  LoginViewController.swift
+//  MusicListApp
+//
+//  Created by Yoshihiro Uda on 2020/02/16.
+//  Copyright © 2020 Yoshihiro Uda. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import DTGradientButton
+
+
+
+class LoginViewController: UIViewController,UITextFieldDelegate{
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.delegate = self
+       
+        //ボタンの背景色
+    loginButton.setGradientBackgroundColors([UIColor(hex:"E21F70"),UIColor(hex:"FF4D2C")], direction: .toBottom, for: .normal)
+        
+        
+    }
+    
+
+    
+    //キーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    
+    @IBAction func login(_ sender: Any) {
+        
+        //textFieldの値が空でない場合、textFieldの値をアプリ内に保存
+        //空ならば振動させる
+        if textField.text?.isEmpty != true{
+            
+            UserDefaults.standard.set(textField.text, forKey: "userName")
+            
+        }else{
+            
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.error)
+            
+        }
+        
+        
+        //firebaseAuthの中にIDと名前(textField.text)を入れる
+        //
+        Auth.auth().signInAnonymously { (result, error) in
+            
+            if error == nil{
+                
+                guard let user = result?.user else{ return }
+                let userID = user.uid
+                UserDefaults.standard.set(userID, forKey: "userID")
+                
+            }else{
+                
+                print(error?.localizedDescription as Any)
+                
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+
+}
---
+++ MusicListApp/Model/SaveProfile.swift
@@ -0,0 +1,61 @@
+//
+//  SaveProfile.swift
+//  MusicListApp
+//
+//  Created by Yoshihiro Uda on 2020/02/17.
+//  Copyright © 2020 Yoshihiro Uda. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import PKHUD
+
+class SaveProfile {
+    
+    //サーバに値を飛ばす
+
+    var userID:String! = ""
+    var userName:String! = ""
+    var ref:DatabaseReference!
+    
+    init(userID:String,userName:String){
+        
+        self.userID = userID
+        self.userName = userName
+        
+        //ログイン時に拾えるuidを先頭につけて送信する。受信する時もuidから引っ張ってくる。
+        
+        ref = Database.database().reference().child("profile").childByAutoId()
+        
+    }
+    
+    init(snapShot:DataSnapshot){
+        
+        ref = snapShot.ref
+        if let value = snapShot.value as? [String:Any]{
+            
+            userID = value["userID"] as? String
+            userName = value["userName"] as? String
+            
+        }
+    
+    }
+    
+    func toContents() -> [String:Any]{
+        
+        return ["userID":userID!,"userName":userName as Any]
+        
+    }
+    
+    
+    func saveProfile(){
+        
+        ref.setValue(toContents())
+        
+    }
+    
+    
+    
+    
+    
+}
