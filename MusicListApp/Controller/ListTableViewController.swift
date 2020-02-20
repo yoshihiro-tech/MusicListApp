01b24234a1808abeacf78d0adbd2f1a6e505608f
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-02-20 22:17:31 +0900

    create GetUserIDModel

--- MusicListApp/Controller/ListTableViewController.swift
+++ MusicListApp/Controller/ListTableViewController.swift
@@ -10,23 +10,61 @@ import UIKit
 import Firebase
 import PKHUD
 
-class ListTableViewController: UIViewController {
-
+class ListTableViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var listRef = Database.database().reference()
+    var indexNumber = Int()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+       
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        
     }
-    */
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        HUD.show(.success)
+        //コンテンツを取得する
+        
+        
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           <#code#>
+       }
+       
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
+   
+    
+    
+    
+    
+    
+    
 
 }
---
+++ MusicListApp/Model/GetUserIDModel.swift
@@ -0,0 +1,9 @@
+//
+//  GetUserIDModel.swift
+//  MusicListApp
+//
+//  Created by Yoshihiro Uda on 2020/02/20.
+//  Copyright © 2020 Yoshihiro Uda. All rights reserved.
+//
+
+import Foundation
